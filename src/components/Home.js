0f445232d0a950e5742c3d7f6376d26355ea4f17
godfrey <showaygodfrey@gmial.com>
2025-01-28 16:48:56 +0300

    made changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-function Home(props) {
+// pass this data down as props to the child component(s) that need it!
+import user from "../data/user";
+
+function App() {
   return (
-    <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
-      </h1>
+    <div>
+      <NavBar />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
 
-export default Home;
+export default App;
